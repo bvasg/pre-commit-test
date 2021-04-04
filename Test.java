c48e4326dbc479308bc4fce2cf056886d92783fd
Blaž Glogovčan <blaz.glogovcan@student.um.si>
2021-04-04 19:09:38 +0200

    Should commit (OG)

--- Test.java
+++ Test.java
@@ -17,7 +17,7 @@ public class test {
     int age = 21;
 
     System.out.println("Whats nine plus ten?");
-    if (9+ 10 == 21) {
+    if (9 + 10 == 21) {
       System.out.println("Twenty-one");
       System.out.println("You stupid!");
     }    
